ecfb275a7227cd0a0193680e04f010c5ec1b6c71
Simranjyot Gill <simranjyot@Simranjyots-MBP.fios-router.home>
2017-07-21 11:06:00 -0400

    fix few bugs

--- battery-plot.py
+++ battery-plot.py
@@ -1,18 +1,27 @@
-import os
+#! /usr/bin/env python3
+import os, math, sys
 import matplotlib.pyplot as plt
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_dir = dir_path + '/.data/'
 files = os.listdir(data_dir)
-file_name = sorted(files)[-1]
 
 x = []
 y = []
-with open(data_dir + file_name, 'r') as handle:
-    for line in handle:
-        data = line.split()
-        x.append(data[0])
-        y.append(data[1])
+MUL = 25
+num_days = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+for index, file_name in enumerate(files[-1*num_days:]):
+    factor = MUL * index
+    with open(data_dir + file_name, 'r') as handle:
+        for line in handle:
+            data = line.split()
+            time_sec = int(data[0])
+            time_hrs = math.floor(time_sec / 3600)
+            time_mins = (time_sec % 3600) / 60
+            scaling_factor = 60
+            time = time_hrs + (float(time_mins) / scaling_factor) + factor
+            x.append(time)
+            y.append(data[1])
 
 plt.plot(x,y)
 plt.show()
--- collect-data.py
+++ collect-data.py
@@ -5,20 +5,24 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_dir = dir_path + '/.data/'
-INTERVAL_SECS = 10
+INTERVAL_SECS = 5
+prev_battery_percentage = ''
 
 def collect_data():
-    current_time = map(int, datetime.now().time().isoformat(timespec='minutes').split(':'))
-    current_time = sum([x*y for x,y in zip(current_time, reversed([60**i for i in range(2)]))])
+    global prev_battery_percentage
+    current_time = map(int, datetime.now().time().isoformat(timespec='seconds').split(':'))
+    current_time = sum([x*y for x,y in zip(current_time, reversed([60**i for i in range(3)]))])
     file_name = '-'.join(map(str, datetime.now().date().timetuple()[:3]))
 
     battery_info = Popen(['pmset', '-g', 'batt'], stdout=PIPE, stderr=STDOUT, encoding='utf8').communicate()[0]
-    battery_percentage = battery_info.split('\n')[1].split('\t')[1][:2]
+    battery_percentage = battery_info.split('\n')[1].split('\t')[1]
+    battery_percentage = battery_percentage[:battery_percentage.index('%')]
+    if battery_percentage != prev_battery_percentage:
+        prev_battery_percentage = battery_percentage
+        data_point = '\t'.join(map(str, [current_time, battery_percentage]))
 
-    data_point = '\t'.join(map(str, [current_time, battery_percentage]))
-
-    with open(data_dir + file_name, 'a') as handle:
-        handle.write(data_point + '\n')
+        with open(data_dir + file_name, 'a') as handle:
+            handle.write(data_point + '\n')
 
     Timer(INTERVAL_SECS, collect_data).start()
 
